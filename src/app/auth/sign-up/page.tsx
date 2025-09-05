87c1cee50e6b83ae568f50ccb2cde991ae51dbd8
mrrahulkrr <rahulkrgupta18032003@gmail.com>
2025-09-05 11:56:24 +0530

    feat: added UI components,  implemented database schema, created custom hooks for mobile detection and authentication, added utility functions for class name, implemented sign-in and sign-up views for authentication

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { SignUpViews } from "@/modules/auth/ui/views/sign-up-views";
+
+const Page = () => {
+  return <SignUpViews />;
+};
+
+export default Page;
---
+++ src/modules/auth/ui/views/sign-up-views.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Card } from "@/components/ui/card";
+
+export const SignUpViews = () => {
+  return (
+    <Card className="p-6 md:p-10 text-center text-2xl md:text-4xl">
+      Sign Up Views
+    </Card>
+  );
+};
